4f05d9970ad25230a22145bc977e637654528496
Sophia Colonello <sophiacolonello@gmail.com>
2025-04-07 10:48:26 -0400

    making button better and dynamic text file

--- app/client/src/Header.js
+++ app/client/src/Header.js
@@ -2,35 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import { useLanguage } from './LanguageContext';
-
-// variable for text -> NEED TO FIGURE OUT HOW TO PUT THIS SOMEWHERE ELSE
-var text= { 
-  "english" : {
-
-      "headerTitle": "Welcome to our Bilingual Gaming Page!",
-      "changeLanguage": "Change Language",
-      "home": "Home",
-      "game1": "Game 1",
-      "game2": "Game 2",
-      "missionStatement": "Mission Statement",
-      "missionStatementText": "This webpage was made to make learning Spanish & English a fun experience for kids! Through these interactive games, kids and people of all ages can build their understanding of a language while having a good time. Our mission is to create a playful learning environment, while still encouraging curiosity and confidence in a new language. By intertwining education with entertainment, this project entices kids to explore bilingualism in a way that makes you feel like you’re playing, not work!"
-
-  },
-
-  "spanish" : {
-
-      "headerTitle": "¡Bienvenido a nuestra página de juegos bilingües!",
-      "changeLanguage": "Cambiar idioma",
-      "home": "Inicio",
-      "game1": "Juego 1",
-      "game2": "Juego 2",
-      "missionStatement": "Declaración de misión",
-      "missionStatementText": "Esta página web fue creada para hacer que aprender español e inglés sea una experiencia divertida para los niños. A través de estos juegos interactivos, los niños y personas de todas las edades pueden construir su comprensión de un idioma mientras se divierten. Nuestra misión es crear un ambiente de aprendizaje lúdico, al mismo tiempo que fomentamos la curiosidad y la confianza en un nuevo idioma. Al entrelazar la educación con el entretenimiento, este proyecto invita a los niños a explorar el bilingüismo de una manera que te hace sentir como si estuvieras jugando, no trabajando."
-
-  }
-
-}
-
+import text from './data.json'
 
 function Header() {
 
@@ -38,14 +10,14 @@ function Header() {
 
   return (
     <div className="header">
-      <h1 className="headerTitle">{text[currentLanguage].headerTitle}</h1>
-      <button onClick = {toggleLanguage}>{text[currentLanguage].changeLanguage}</button>
+      <h1 className="headerTitle">{text[currentLanguage].header.headerTitle}</h1>
       <nav className="navbar">
+      <button className= "languageSelectorButton" onClick = {toggleLanguage}>{text[currentLanguage].header.changeLanguage}</button>
         <ul className="navbarList">
-          <li><Link to="/home">{text[currentLanguage].home}</Link></li>
-          <li><Link to="/game1">{text[currentLanguage].game1}</Link></li>
-          <li><Link to="/game2">{text[currentLanguage].game2}</Link></li>
-          <li><Link to="/MissionStatement">{text[currentLanguage].missionStatement}</Link></li>
+          <li><Link to="/home">{text[currentLanguage].header.home}</Link></li>
+          <li><Link to="/game1">{text[currentLanguage].header.game1}</Link></li>
+          <li><Link to="/game2">{text[currentLanguage].header.game2}</Link></li>
+          <li><Link to="/MissionStatement">{text[currentLanguage].header.missionStatement}</Link></li>
         </ul>
       </nav>
     </div>
--- app/client/src/Home.js
+++ app/client/src/Home.js
@@ -3,26 +3,7 @@ import React from 'react';
 import './Home.css';
 import { useNavigate } from 'react-router-dom';
 import { useLanguage } from './LanguageContext'; // Import useLanguage from LanguageContext.js
-
-
-var text = {
-  "english": {
-    "homeTitle": "Welcome to our Bilingual Gaming Page!",
-    "homeDescription": "This webpage was made to make learning Spanish & English a fun experience for kids! Through these interactive games, kids and people of all ages can build their understanding of a language while having a good time.",
-    "homeDescription2": "Our mission is to create a playful learning environment, while still encouraging curiosity and confidence in a new language. By intertwining education with entertainment, this project entices kids to explore bilingualism in a way that feels like playing—not work!",
-    "game1": "Word Box",
-    "game2": "Word Search",
-    "click": "Click here to play!",
-  },
-  "spanish": {
-    "homeTitle": "¡Bienvenido a nuestra página de juegos bilingües!",
-    "homeDescription": "Esta página web fue creada para hacer que aprender español e inglés sea una experiencia divertida para los niños. A través de estos juegos interactivos, los niños y personas de todas las edades pueden construir su comprensión de un idioma mientras se divierten.",
-    "homeDescription2": "Nuestra misión es crear un ambiente de aprendizaje lúdico, al mismo tiempo que fomentamos la curiosidad y la confianza en un nuevo idioma. Al entrelazar la educación con el entretenimiento, este proyecto invita a los niños a explorar el bilingüismo de una manera que te hace sentir como si estuvieras jugando, no trabajando.",
-    "game1": "Caja de palabras",
-    "game2": "Búsqueda de palabras",
-    "click": "¡Haz clic aquí para jugar!",
-  }
-  }
+import text from './data.json'
 
  
 function Home() {
@@ -31,24 +12,24 @@ function Home() {
 
   return (
     <div className="homePage">
-      <h1 className="homeTitle">{text[currentLanguage].homeTitle}</h1>
-      <p className="homeDescription">{text[currentLanguage].homeDescription}</p>
-      <p className="homeDescription">{text[currentLanguage].homeDescription2}</p>
+      <h1 className="homeTitle">{text[currentLanguage].home.homeTitle}</h1>
+      <p className="homeDescription">{text[currentLanguage].home.homeDescription}</p>
+      <p className="homeDescription">{text[currentLanguage].home.homeDescription2}</p>
 
       <div className="gameSelection">
         <div
           className="gameBox game1"
           onClick={() => navigate('/game1')}
         >
-          <h2>{text[currentLanguage].game1}</h2>
-          <p>{text[currentLanguage].click}</p>
+          <h2>{text[currentLanguage].home.game1}</h2>
+          <p>{text[currentLanguage].home.click}</p>
         </div>
         <div
           className="gameBox game2"
           onClick={() => navigate('/game2')}
         >
-          <h2>{text[currentLanguage].game2}</h2>
-          <p>{text[currentLanguage].click}</p>
+          <h2>{text[currentLanguage].home.game2}</h2>
+          <p>{text[currentLanguage].home.click}</p>
         </div>
       </div>
     </div>
