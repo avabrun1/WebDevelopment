828cdb2a190516b7acc57ea25f3f8135c6cbf344
avabrun1 <159275622+avabrun1@users.noreply.github.com>
2025-04-15 10:16:09 -0400

    missionStatementPage so far

--- app/client/src/MissionStatement.js
+++ app/client/src/MissionStatement.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './MissionStatement.css';
 
 function MissionStatement() {
     return(
@@ -7,6 +8,23 @@ function MissionStatement() {
         <p>
         This webpage was made to make learning Spanish & English a fun experience for kids! Through these interactive games, kids and people of all ages can build their understanding of a language while having a good time. Our mission is to create a playful learning environment, while still encouraging curiosity and confidence in a new language. By intertwining education with entertainment, this project entices kids to explore bilingualism in a way that makes you feel like you’re playing, not work!
         </p>
+        <h2>Our Front End Team</h2>
+        <ul>
+            <li>
+            <img src="/brunerHeadshot.jpg" alt="Ava Bruner" />
+            Majoring in Computer Science, <strong>Ava Bruner</strong> has gained extensive experience with a variety of programming languages and tools 
+            including SQL, Java, Python, HTML, and CSS. Currently, she is serving JCU at 
+            the IT Service Desk as a Hardware Technician, and Cleveland Clinic as a Software Migration Intern. 
+            These experiences equip her with the technical knowledge and organizational skills needed to effeciently lead a dev-team to build a successful educational website.
+             After graduation in May 2025, she will be working at Union Home Mortgage Corp. as an IT Project Coordinator intern and working towards her CompTIA A+ certification.
+            </li>
+            <li>
+                Thaddeus Salwan
+            </li>
+            <li>
+                Thomas Richardson
+            </li>
+        </ul>
         </div>
     )
 }
