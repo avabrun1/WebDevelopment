e3b2c1a0a82d5858053f0ce08d2055bbdffeca57
tsal4 <159286965+tsal4@users.noreply.github.com>
2025-04-23 19:17:56 -0400

    added one indent and some spacing in between html elements

--- app/client/src/MissionStatement.js
+++ app/client/src/MissionStatement.js
@@ -4,40 +4,43 @@ import './MissionStatement.css';
 function MissionStatement() {
     return(
         <div className="missionStatementPage">
-        <h1>Mission Statement</h1>
-        <p>
-        This webpage was made to make learning Spanish & English a fun experience for kids! Through these interactive games, kids and people of all ages can build their understanding of a language while having a good time. Our mission is to create a playful learning environment, while still encouraging curiosity and confidence in a new language. By intertwining education with entertainment, this project entices kids to explore bilingualism in a way that makes you feel like you’re playing, not work!
-        </p>
-        <h2>Advisory</h2>
-        <ul>
-            <li>Dr. Osvaldo Tapia-Duenas</li>
-            <li>Chelsea Calalb</li>
-        </ul>
-        <h2>Our Front End Team</h2>
-        <ul>
-            <li>
-            {/* <img src="app/client/public/brunerHeadshot.jpg" alt="Ava Bruner" /> */}
-            Majoring in Computer Science, <strong>Ava Bruner</strong> has gained extensive experience with a variety of programming languages and tools 
-            including SQL, Java, Python, HTML, and CSS. Currently, she is serving JCU at 
-            the IT Service Desk as a Hardware Technician, and Cleveland Clinic as a Software Migration Intern. 
-            These experiences equip her with the technical knowledge and organizational skills needed to effeciently lead a dev-team to build a successful educational website.
-             After graduation in May 2025, she will be working at Union Home Mortgage Corp. as an IT Project Coordinator intern and working towards her CompTIA A+ certification.
-            </li>
-            <li>
-                Thaddeus Salwan
-            </li>
-            <li>
-                Thomas Richardson
-            </li>
-        </ul>
-        <h2>
-            Our Back End Team
-        </h2>
-        <ul>
-            <li>Sophia Colonello</li>
-            <li>Yoni Sabag</li>
-            <li>Riley McGinty</li>
-        </ul>
+            <h1>Mission Statement</h1>
+            <p>
+            This webpage was made to make learning Spanish & English a fun experience for kids! Through these interactive games, kids and people of all ages can build their understanding of a language while having a good time. Our mission is to create a playful learning environment, while still encouraging curiosity and confidence in a new language. By intertwining education with entertainment, this project entices kids to explore bilingualism in a way that makes you feel like you’re playing, not work!
+            </p>
+
+            <h2>Advisory</h2>
+            <ul>
+                <li>Dr. Osvaldo Tapia-Duenas</li>
+                <li>Chelsea Calalb</li>
+            </ul>
+
+            <h2>Our Front End Team</h2>
+            <ul>
+                <li>
+                {/* <img src="app/client/public/brunerHeadshot.jpg" alt="Ava Bruner" /> */}
+                Majoring in Computer Science, <strong>Ava Bruner</strong> has gained extensive experience with a variety of programming languages and tools 
+                including SQL, Java, Python, HTML, and CSS. Currently, she is serving JCU at 
+                the IT Service Desk as a Hardware Technician, and Cleveland Clinic as a Software Migration Intern. 
+                These experiences equip her with the technical knowledge and organizational skills needed to effeciently lead a dev-team to build a successful educational website.
+                After graduation in May 2025, she will be working at Union Home Mortgage Corp. as an IT Project Coordinator intern and working towards her CompTIA A+ certification.
+                </li>
+                <li>
+                    Thaddeus Salwan
+                </li>
+                <li>
+                    Thomas Richardson
+                </li>
+            </ul>
+            
+            <h2>
+                Our Back End Team
+            </h2>
+            <ul>
+                <li>Sophia Colonello</li>
+                <li>Yoni Sabag</li>
+                <li>Riley McGinty</li>
+            </ul>
         </div>
     )
 }
