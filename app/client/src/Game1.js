913b48f000480414068f0883369b3eda8b826bcf
avabrun1 <159275622+avabrun1@users.noreply.github.com>
2025-04-23 12:07:59 -0400

    game1 and game2 css styling

--- app/client/src/Game1.js
+++ app/client/src/Game1.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import './Home.css';
+import './Game1.css';  
 import { useNavigate } from 'react-router-dom';
-import { useLanguage } from './LanguageContext'; // Import useLanguage from LanguageContext.js
-import text from './data.json'
+import { useLanguage } from './LanguageContext'; 
+import text from './data.json';
 
- 
 function Game1() {
   const navigate = useNavigate();
-  const { currentLanguage } = useLanguage(); // Use the custom hook to get the current language
+  const { currentLanguage } = useLanguage(); 
 
   return(
-    <div>
+    <div className="game1Page">
         <h2>{text[currentLanguage].game1.name}</h2>
+        <h1>Description</h1>
         <p>{text[currentLanguage].game1.description}</p>
+        <h1>Instructions</h1>
         <p>{text[currentLanguage].game1.instructions}</p>
+        <h1>Scoring</h1>
         <p>{text[currentLanguage].game1.scoring}</p>
     </div>
-  )
+  );
 }
 
 export default Game1;
--- app/client/src/Game2.js
+++ app/client/src/Game2.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Game2.css';
 import './Home.css';
 import { useNavigate } from 'react-router-dom';
 import { useLanguage } from './LanguageContext'; // Import useLanguage from LanguageContext.js
@@ -10,12 +11,16 @@ function Game2() {
   const { currentLanguage } = useLanguage(); // Use the custom hook to get the current language
 
   return(
-    <div>
+    <div className="game2Page">
         <h2>{text[currentLanguage].game2.name}</h2>
+        <h1>Description</h1>
         <p>{text[currentLanguage].game2.description}</p>
+        <h1>Instructions</h1>
         <p>{text[currentLanguage].game2.instructions}</p>
+        <h1>Scoring</h1>
+        <p>{text[currentLanguage].game2.scoring}</p>
     </div>
-  )
+  );
 }
 
 export default Game2;
