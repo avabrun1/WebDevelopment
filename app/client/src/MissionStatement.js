32e1e861579425fba5e91d43d15c310e5ce498a1
avabrun1 <159275622+avabrun1@users.noreply.github.com>
2025-05-01 19:55:12 -0400

    updated mission statement page

--- app/client/src/MissionStatement.js
+++ app/client/src/MissionStatement.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import './MissionStatement.css';
+import ava from './assets/brunerHeadshot.jpg';
+import chelsea from './assets/calalbHeadshot.pdf';
+import sophia from './assets/colonelloHeadshot.pdf';
+import yoni from './assets/sabagHeadshot.webp';
+import riley from './assets/mcgintyHeadshot.pdf';
+import tad from './assets/salwanHeadshot.jpeg';
+import thomas from './assets/richardsonHeadshot.jpeg';
 
 function MissionStatement() {
     return(
@@ -18,21 +25,24 @@ function MissionStatement() {
             <h2>Our Front End Team</h2>
             <ul>
                 <li>
-                    {/* <img src="app/client/public/brunerHeadshot.jpg" alt="Ava Bruner" /> */}
-                    Majoring in Computer Science, <strong>Ava Bruner</strong> has gained extensive experience with a variety of programming languages and tools 
+                    <img src={ava} alt="Ava Bruner" className="missionStatementPhotos" />
+                    <p><strong>Ava Bruner</strong> is majoring in Computer Science, and has gained extensive experience with a variety of programming languages and tools 
                     including SQL, Java, Python, HTML, and CSS. Currently, she is serving JCU at 
-                    the IT Service Desk as a Hardware Technician, and Cleveland Clinic as a Software Migration Intern. 
-                    These experiences equip her with the technical knowledge and organizational skills needed to effeciently lead a dev-team to build a successful educational website.
-                    After graduation in May 2025, she will be working at Union Home Mortgage Corp. as an IT Project Coordinator intern and working towards her CompTIA A+ certification.
+                    the IT Service Desk as a Hardware Technician, and Cleveland Clinic as a Software Development Intern. 
+                    These experiences equip her with the technical knowledge and organizational skills needed to efficiently lead a dev-team to build a successful educational website.
+                    After graduation in May 2025, she will be working at Union Home Mortgage Corp. as an IT Project Coordinator and working towards her CompTIA A+ certification.</p>
                 </li>
                 <li>
-                    {/* <img src="app/client/public/salwanHeadshot.pdf" alt="Thaddeus Salwan" /> */}
-                    <strong>Thaddeus Salwan</strong> is a third year Computer Science student at John Carroll University. He is also minoring in both Business and Data Science.
+                    <img src={tad} alt="Thaddeus Salwan" className="missionStatementPhotos" />
+                    <p><strong>Thaddeus Salwan</strong> is a third year Computer Science student at John Carroll University. He is also minoring in both Business and Data Science.
                     He has experience in programming languages such as Python, Java, HTML, CSS, and SQL. During the Summer of 2025 he will be working at Sherwin Williams
-                    as an IT Intern.
+                    as an IT Intern.</p>
                 </li>
                 <li>
-                    Thomas Richardson
+                    <img src={thomas} alt="Thomas Richardson" className="missionStatementPhotos" />
+
+                <p><strong>Thomas Richardson III</strong> is a Junior Computer Science major and Philosophy minor at John Carroll University. He has experience with different coding 
+                languages like Python, Java, and HTML, with some added experience with Data Science subjects. He is a football player at JCU and is involved in various organizations.</p>
                 </li>
             </ul>
             
@@ -40,8 +50,11 @@ function MissionStatement() {
                 Our Back End Team
             </h2>
             <ul>
-                <li>Sophia Colonello</li>
-                <li>Yoni Sabag</li>
+                <li>
+                    Sophia Colonello</li>
+                <li>
+                    <img src={yoni} alt="Yoni Sabag" className="missionStatementPhotos" />
+                    Yoni Sabag</li>
                 <li>Riley McGinty</li>
             </ul>
         </div>
