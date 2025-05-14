63fe507d05b693661bde13a3f0ef5cc437bb1654
tsal4 <159286965+tsal4@users.noreply.github.com>
2025-05-14 11:52:00 -0400

    changed footer.css selectors to be class selectors instead of type selectors

--- app/client/src/Footer.js
+++ app/client/src/Footer.js
@@ -9,18 +9,18 @@ function Footer() {
     return (
       <footer className="footer">
         <div>
-          <img src={jcu_logo} alt="John Carroll University logo"/>
+          <img src={jcu_logo} alt="John Carroll University logo" className="footerImage"/>
           <a href="https://www.jcu.edu/" target="_blank" rel="noopener noreferrer" className="jcuLink">Visit jcu.edu</a>
           <p>&copy; For inquiries, contact otapiaduenas@jcu.edu.</p>
         </div>
         <div>
           <h3>Stay Connected with JCU</h3>
-              <a href="https://www.facebook.com/JCU1886" target="_blank" rel="noopener noreferrer">Facebook</a>
-              <a href="https://www.linkedin.com/school/john-carroll-university/posts/?feedView=all" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-              <a href="https://www.tiktok.com/@johncarrolluniversity" target="_blank" rel="noopener noreferrer">TikTok</a>
-              <a href="https://x.com/johncarrollu" target="_blank" rel="noopener noreferrer">X</a>
-              <a href="https://www.instagram.com/johncarrollu/" target="_blank" rel="noopener noreferrer">Instagram</a>
-              <a href="https://www.youtube.com/user/JohnCarrollU" target="_blank" rel="noopener noreferrer">Youtube</a>
+              <a href="https://www.facebook.com/JCU1886" target="_blank" rel="noopener noreferrer" className="socialLink">Facebook</a>
+              <a href="https://www.linkedin.com/school/john-carroll-university/posts/?feedView=all" target="_blank" rel="noopener noreferrer" className="socialLink">LinkedIn</a>
+              <a href="https://www.tiktok.com/@johncarrolluniversity" target="_blank" rel="noopener noreferrer" className="socialLink">TikTok</a>
+              <a href="https://x.com/johncarrollu" target="_blank" rel="noopener noreferrer" className="socialLink">X</a>
+              <a href="https://www.instagram.com/johncarrollu/" target="_blank" rel="noopener noreferrer" className="socialLink">Instagram</a>
+              <a href="https://www.youtube.com/user/JohnCarrollU" target="_blank" rel="noopener noreferrer" className="socialLink">Youtube</a>
         </div>
       </footer>
     );
